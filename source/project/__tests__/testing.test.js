9ec751c8ba5eb79ceea38ce17e4bef9e6cf717a3
mapersiani <mapersiani@users.noreply.github.com>
2024-05-15 23:13:38 +0000

    Apply Prettier changes

--- source/project/__tests__/testing.test.js
+++ source/project/__tests__/testing.test.js
@@ -1,9 +1,9 @@
-import testing from '../scripts/testing';
+import testing from "../scripts/testing";
 
-test('Sanity Testing Part 2', () => {
-  expect(testing('test')).toBe('test testing');
+test("Sanity Testing Part 2", () => {
+  expect(testing("test")).toBe("test testing");
 });
 
-test('Sanity Testing Part 3', () => {
-  expect(testing('test')).toBe('test testing');
+test("Sanity Testing Part 3", () => {
+  expect(testing("test")).toBe("test testing");
 });
