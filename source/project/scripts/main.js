fc921b74215f36348a4fbf91ca6ec47a1c0c08a3
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-05-18 22:24:09 -0700

    ElectronJS base

---
+++ source/project/scripts/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path');
+
+const createWindow = () => {
+  let __dirname = path.resolve();
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'source/project/scripts/preload.js'),
+        nodeIntegration: true,
+        contextIsolation: true,
+    }
+  })
+
+  win.loadFile('./source/project/pages/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
