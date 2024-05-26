02b83b811c99b5129752cfbf823f223c6168ae5c
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-05-26 00:39:24 -0700

    Electron testing

---
+++ source/project/__tests__/playwright/electron.test.js
@@ -0,0 +1,18 @@
+import { test, _electron as electron } from '@playwright/test';
+import { expect } from '@playwright/test';
+
+const mainLocation = './source/project/scripts/main.cjs'
+
+test('App launches and can get isPackaged', async () => {
+  const electronApp = await electron.launch({ args: [mainLocation] });
+  const isPackaged = await electronApp.evaluate(async ({ app }) => {return app.isPackaged});
+  expect(isPackaged).toBe(false);
+  await electronApp.close();
+});
+
+test('Window opens', async () => {
+  const electronApp = await electron.launch({ args: [mainLocation] });
+  const window = await electronApp.firstWindow();
+  expect(window).toBeTruthy();
+  await electronApp.close();
+});
