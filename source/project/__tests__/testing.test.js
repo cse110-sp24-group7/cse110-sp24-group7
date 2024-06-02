bbff766be6498c35c1f5ce2b65b1fe901bf1499f
SB1019 <sasrinath@ucsd.edu>
2024-06-02 11:45:51 -0700

    Prettier Fixing. Added Package.json dependencies.

--- source/project/__tests__/testing.test.js
+++ source/project/__tests__/testing.test.js
@@ -1,9 +1,9 @@
-const testing = require('../scripts/testing');
+const testing = require("../scripts/testing");
 
-test('Sanity Testing Part 2', () => {
-  expect(testing('test')).toBe('test testing');
+test("Sanity Testing Part 2", () => {
+	expect(testing("test")).toBe("test testing");
 });
 
-test('Sanity Testing Part 3', () => {
-  expect(testing('test')).toBe('test testing');
+test("Sanity Testing Part 3", () => {
+	expect(testing("test")).toBe("test testing");
 });
