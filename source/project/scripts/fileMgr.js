0c7c54d54a18f681c8f6efc24bd5373981a0a48d
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-06-06 23:39:03 -0700

    Add comments

--- source/project/scripts/fileMgr.js
+++ source/project/scripts/fileMgr.js
@@ -1,6 +1,15 @@
 const path = require("path");
 const fs = require('fs');
+/**
+ * @module dbMgr
+ * @description This module is responsible for managing files, with the ability to add & remove at the location provided.
+ */
 
+/**
+ * @description Encapsulates functions for managing files
+ * @param {string} data_location - the path to the directory that the file operations will be carried out.
+ * @returns {object}
+ */
 const FileManager = (data_location) => {
     const route = path.join(data_location, 'assets');
     if (!fs.existsSync(route)) fs.mkdirSync(route);
@@ -11,13 +20,24 @@ const FileManager = (data_location) => {
     const file_route = path.join(route, 'files');
     if (!fs.existsSync(file_route)) fs.mkdirSync(file_route);
 
-    //check if file exists 
+    /**
+     * @description Checks if file exists in the directory
+     * @param {string} file_name - the name of the file
+     * @param {boolean} is_img - whether the file is an image
+     * @returns {boolean}
+     
+    */
     const exists = (file_name, is_img=false) => {
         const location = getFileLocation(file_name, is_img);
         return fs.existsSync(location);
     }
 
-    //check where a file with its name and type would be located
+     /**
+     * @description Check where the file would be located in the directory
+     * @param {string} file_name - the name of the file 
+     * @param {boolean} is_img - whether the file is an image
+     * @returns {string}
+    */
     const getFileLocation = (file_name, is_img=false) => {
         let fileLocation;
         if(is_img) fileLocation = path.join(img_route, file_name);
@@ -25,24 +45,43 @@ const FileManager = (data_location) => {
         return fileLocation;
     }
 
-    //get list of file names
+    /**
+     * @description Get the complete list of non-image file names
+     * @returns {string[]}
+
+    */
     const getFileNames = () => {
         return fs.readdirSync(file_route);
     } 
 
-    //get list of image names
+    /**
+     * @description Get the complete list of image names
+     * @returns {string[]}
+
+    */
     const getImageNames = () => {
         return fs.readdirSync(img_route);
     } 
     
-    //get file contents
+    /**
+     * @description Get the contents of the file
+     * @param {string} file_name - the name of the file 
+     * @param {boolean} is_img - whether the file is an image
+     * @returns {string}
+
+    */
     const getContent = async (file_name, is_img=false) => {
         const location = getFileLocation(file_name, is_img);
         const blob = new Blob([fs.readFileSync(location)]);
         return await blob.text();
     }
     
-    //add file to directory
+    /**
+     * @description Add a file to the directory
+     * @param {file} file - a JS file object to be copied
+     * @returns {void}
+
+    */
     const add = (file) => {
         const is_img = file.type.includes('image');
         const fileLocation = getFileLocation(file.name, is_img);
@@ -55,7 +94,13 @@ const FileManager = (data_location) => {
         }
     }
     
-    //remove file from directory
+    /**
+     * @description Removes the file from the directory
+     * @param {string} file_name - the name of the file 
+     * @param {boolean} is_img - whether the file is an image
+     * @returns {Promise}
+
+    */
     const remove = (file_name, is_img=false) => {
         const fileLocation = getFileLocation(file_name, is_img)
         if (!fs.existsSync(fileLocation)) return {errorCode: 404, errorMsg: `A file with the name ${file_name} was not found. No files were deleted.`};
