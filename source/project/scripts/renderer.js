6db8c2a8cc7d1625efcf959fae8280285ac9a899
andronius-yang <androniusyang@gmail.com>
2024-06-08 10:13:10 -0700

    Cleanded up comments and removed unused files.

--- source/project/scripts/renderer.js
+++
@@ -1,7 +0,0 @@
-/**
- * @module Renderer
- * @description This module is responsible for rendering the app and handling launching the app window and loading the app.
- */
-/* eslint-disable no-unused-vars */
-const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-const journals = JSON.parse(localStorage.getItem("journalData") || "[]");
